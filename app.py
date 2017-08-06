3d4921e9eb50017d058520e243d84ec98452decb
Amir Hozez <amir@intsights.com>
2017-08-06 16:57:56 +0300

    WIP

--- app.py
+++ app.py
@@ -68,4 +68,7 @@ def analyze_sentence():
 @flask_app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+
     return response
