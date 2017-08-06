a81647aab03b4b56fb15bcf5035a67d1340bee15
Amir Hozez <amir@intsights.com>
2017-08-06 14:18:36 +0300

    Renamed regex file

--- nlp.py
+++ nlp.py
@@ -2,7 +2,7 @@ import spacy
 import spacy.symbols # nsubj, VERB, dobj
 
 nlp = spacy.load('en')
-doc = nlp(u'The malware writes the file wannacry.exe to the server')
+doc = nlp(u'The malware does not write the file wannacry.exe to the server')
 
 def get_verb_ancestors(
     token,
@@ -31,6 +31,14 @@ def traverse_verb_ancestors(
         ancestors,
     )
 
+def is_verb_negated(
+    token,
+):
+    for child in token.children:
+        if child.dep_ == 'neg' and child.pos_ == 'ADV':
+            return True
+
+    return False
 
 def is_valid_candidate(
     np,
@@ -50,12 +58,13 @@ def is_valid_candidate(
         'patch',
         'release',
     ]
-
+    import pudb; pudb.set_trace()
     allowed_dependency = np.dep_ in allowed_deps
     verb_ancestors = get_verb_ancestors(np)
 
     has_whitelisted_ancestor_verbs = False
     has_ioc_related_ancestor_verbs = False
+    is_any_verb_negated = False
     for verb_ancestor in verb_ancestors:
         is_whitelisted_verb = verb_ancestor.lemma_ in whiltelisted_verbs
         if is_whitelisted_verb:
@@ -65,16 +74,17 @@ def is_valid_candidate(
         if is_ioc_related_verb:
             has_ioc_related_ancestor_verbs = True
 
+        is_any_verb_negated = is_verb_negated(verb_ancestor)
+
     if not allowed_dependency or has_whitelisted_ancestor_verbs:
         return False
 
-    if allowed_dependency and has_ioc_related_ancestor_verbs:
+    if allowed_dependency and has_ioc_related_ancestor_verbs and not is_any_verb_negated:
         return True
 
 
+# get_y
 for np in doc:
-    # print('np.text: {}, np.dep_: {}, np.pos_: {}, np.head.text: {}, np.head.dep_: {}, np.head.pos_: {}, np.lemma_: {}'.format(
-    #     np.text, np.dep_, np.pos_, np.head.text, np.head.dep_, np.head.pos_, np.lemma_
-    # ))
+    # 
     if np.text == 'wannacry.exe':
         print(is_valid_candidate(np))
