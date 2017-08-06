2167cfa7c79579ab941da9954d0888ab20281133
Maxime Carlier <maxime@intsights.com>
2017-08-06 16:34:30 +0300

    make the gunicorn.sh Windows compatible

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-gunicorn \
-        -b 0.0.0.0:8080 \
-        app:flask_app \
-        --access-logfile=/tmp/gunicorn.log \
-        --pid /tmp/gunicorn.pid \
-        -w 5 \
-        --threads 1 \
-        --timeout 900
+gunicorn -b 0.0.0.0:8080 app:flask_app --access-logfile=/tmp/gunicorn.log --pid /tmp/gunicorn.pid -w 5 --threads 1 --timeout 900
